511ef2a1b1010bd5522a0e37339cc22515fa8941
Kiri-ship <Onkill249@gmail.com>
2024-09-25 11:16:57 +0300

    Add files via upload

--- calculator_Not_Bag.go
+++ calculator_Not_Bag.go
@@ -162,15 +162,16 @@ func hundlerArabic(x, y int) {
 func hundlerRome(x, y string) {
 
 	RomeToArab := map[string]int{
-		"I":   1,
-		"II":  2,
-		"III": 3,
-		"IV":  4,
-		"V":   5,
-		"VI":  6,
-		"VII": 7,
-		"IX":  9,
-		"X":   10,
+		"I":    1,
+		"II":   2,
+		"III":  3,
+		"IV":   4,
+		"V":    5,
+		"VI":   6,
+		"VII":  7,
+		"VIII": 8,
+		"IX":   9,
+		"X":    10,
 	}
 
 	if znac == "+" {
@@ -203,11 +204,7 @@ func hundlerRome(x, y string) {
 
 		c := (RomeToArab[x] * RomeToArab[y])
 
-		
-
-			translateToRome(c)
-
-		
+		translateToRome(c)
 
 	}
 	if znac == "/" {
